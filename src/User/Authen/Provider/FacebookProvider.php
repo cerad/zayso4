3db83598b3b2f8b93bd9f75e79ff6ffccee4ba1d
ahundiak <ahundiak@gmail.com>
2018-11-12 14:00:29 -0600

    Facebook tweak

--- src/User/Authen/Provider/FacebookProvider.php
+++ src/User/Authen/Provider/FacebookProvider.php
@@ -6,15 +6,8 @@ class FacebookProvider extends AbstractProvider
     protected $state = 'facebook';
     protected $scope = 'email';
 
-    protected $userInfoUrl      = 'https://graph.facebook.com/me';
+    protected $userInfoUrl      = 'https://graph.facebook.com/me?fields=email,name';
     protected $accessTokenUrl   = 'https://graph.facebook.com/oauth/access_token';
     protected $revokeTokenUrl   = 'https://graph.facebook.com/me/permissions';
     protected $authorizationUrl = 'https://www.facebook.com/dialog/oauth';
-
-    /*
-     * When using http local host facebook no longer returns email
-     * Just name and id
-     *
-     * Facebook has detected Zayso isn't using a secure connection to transfer information.
-     */
 }
